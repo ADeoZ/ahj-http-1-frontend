f2cce7e1ac75f53a60efa9d0f3dbb31a902f839d
Aleksey Denisov <deoz@yandex.ru>
2021-05-29 20:35:09 +0300

    Init

---
+++ src/js/app.js
@@ -0,0 +1,44 @@
+// TODO: write code here
+
+const server = 'http://ahj-http-1.herokuapp.com/';
+// const server = 'http://localhost:7070/';
+
+const clickme = document.querySelector('.test');
+clickme.addEventListener('click', (evt) => {
+  // const params = 'method=ticketById&id=4';
+  const params = 'method=allTickets';
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `${server}?${params}`);
+  xhr.addEventListener('load', () => {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      try {
+        console.log(xhr.response);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+  xhr.send();
+});
+
+const postButton = document.querySelector('.post');
+postButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  const params = new URLSearchParams();
+
+  params.append('name', 'Тестовый тикет');
+  params.append('description', 'Пробуем загрузить новый тикет и получить по нему информацию');
+  params.append('status', 0);
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', `${server}?method=createTicket`);
+  xhr.addEventListener('load', () => {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      try {
+        console.log(xhr.response);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+  xhr.send(params);
+});
